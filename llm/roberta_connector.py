baa1d702cfb26c11aeb5d0fb480d3e6bfb1abc4d
Marcos Deus <87666623+Marcos574@users.noreply.github.com>
2024-09-23 23:51:50 -0300

    Fix roberta conector
    Exclude context of roberta conector

--- llm/roberta_connector.py
+++ llm/roberta_connector.py
@@ -4,11 +4,12 @@ from llm.llm_connector import LLMConnector
 class RoBERTaConnector(LLMConnector):
     def __init__(self):
         self.model = None
+        self.default_context = "Este é um modelo de linguagem treinado para responder perguntas."
 
     def connect(self):
         print("Carregando o modelo RoBERTa para perguntas e respostas...")
         self.model = pipeline("question-answering", model="deepset/roberta-base-squad2")
 
-    def generate_response(self, question: str, context: str) -> str:
-        result = self.model(question=question, context=context)
+    def generate_response(self, question: str) -> str:
+        result = self.model(question=question, context=self.default_context)
         return result['answer']
